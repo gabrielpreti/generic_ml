9c133d220fc70b49d83793ff1ada6e344fcb21e6
Preti <gsantiago@uolinc.com>
2015-01-18 22:15:09 -0200

    melhorias no paralelismo

--- src/main/java/com/ml/v2/integ/AccessLogEventsProducer.java
+++ src/main/java/com/ml/v2/integ/AccessLogEventsProducer.java
@@ -3,21 +3,28 @@ package com.ml.v2.integ;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.CsvDataFormat;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class AccessLogEventsProducer extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
         CsvDataFormat csv = new CsvDataFormat("\t");
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
         from("file:/tmp/load/?delete=true&preMove=.run").//
                 id("eventsProducer").//
                 autoStartup(true).//
                 split(body().tokenize("\n")).//
+                executorService(threadPool).//
                 streaming().//
-                threads(10).//
                 unmarshal(csv).//
                 process(new AccessLogEventProcessor()).//
-                beanRef("httpEventsRepository", "addEvent").log("result is ${body}");
+                beanRef("httpEventsRepository", "addEvent").//
+                log("result is ${body}").//
+                end().//
+                log("Done");
     }
 
 }
